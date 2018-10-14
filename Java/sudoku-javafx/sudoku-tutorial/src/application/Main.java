4674f4aff05cfd9f96d38bd435fa49d854b308cf
Zheyin Zeng <u6545267@anu.edu.au>
2018-10-14 21:42:18 +1100

    Automatically optimized code with Intellij

--- Java/sudoku-javafx/sudoku-tutorial/src/application/Main.java
+++ Java/sudoku-javafx/sudoku-tutorial/src/application/Main.java
@@ -2,9 +2,9 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /***
  * The application class; handles inflating the layout and launching itself.
@@ -17,29 +17,29 @@ import javafx.scene.Scene;
  * @see Stage
  */
 public class Main extends Application {
-	@Override
-	/* modify the method declaration to throw generic Exception (in case any of the steps fail) */
-	public void start(Stage primaryStage) throws Exception {
+    /***
+     *
+     * @param args to configure launch with
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-		/* load layout.fxml from file and assign it to a scene root object */
-		Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+    @Override
+    /* modify the method declaration to throw generic Exception (in case any of the steps fail) */
+    public void start(Stage primaryStage) throws Exception {
 
-		/* assign the root to a new scene and define its dimensions */
-		Scene scene = new Scene(root, 720, 480);
+        /* load layout.fxml from file and assign it to a scene root object */
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
 
-		/* set the title of the stage (window) */
-		primaryStage.setTitle("Sudoku");
-		/* set the scene of the stage to our newly created from the layout scene */
-		primaryStage.setScene(scene);
-		/* show the stage */
-		primaryStage.show();
-	}
+        /* assign the root to a new scene and define its dimensions */
+        Scene scene = new Scene(root, 720, 480);
 
-	/***
-	 *
-	 * @param args to configure launch with
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+        /* set the title of the stage (window) */
+        primaryStage.setTitle("Sudoku");
+        /* set the scene of the stage to our newly created from the layout scene */
+        primaryStage.setScene(scene);
+        /* show the stage */
+        primaryStage.show();
+    }
 }
